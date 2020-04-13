fbc3454ccf3d2a6a58d3e674b75bff96d5f7c03e
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-13 12:31:29 +0000

    Added single entry to the new table

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -15,11 +15,9 @@ connection = pymysql.connect(host='localhost',
 try:
     #Run a query
     with connection.cursor() as cursor:
-        cursor.execute(""" CREATE TABLE IF NOT EXISTS
-                        Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error)
-        for row in cursor:
-            print(row)
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT InTO Friends VALUES (%s, %s,%s);",row)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
