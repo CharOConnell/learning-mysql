5468dcf0322bc053e06902a81d6363c4b88bc5f6
CharOConnell <charlotteoconnell@virginmedia.com>
2020-04-13 12:31:05 +0000

    Add a new table

--- mysql_from_python.py
+++ mysql_from_python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from the workspace
@@ -13,9 +14,10 @@ connection = pymysql.connect(host='localhost',
 
 try:
     #Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
+    with connection.cursor() as cursor:
+        cursor.execute(""" CREATE TABLE IF NOT EXISTS
+                        Friends(name char(20), age int, DOB datetime);""")
+        # Note that the above will still display a warning (not error)
         for row in cursor:
             print(row)
 finally:
